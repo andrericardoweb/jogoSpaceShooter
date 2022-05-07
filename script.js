42d9c59e931aa5dd428cf65ccb87b73fe825a015
andrericardoweb <andrericardoweb@gmail.com>
2022-05-07 10:54:46 -0300

    moveUp and moveDown do herói

---
+++ script.js
@@ -0,0 +1,44 @@
+const yourShip = document.querySelector(".player-shooter");
+const playArea = document.querySelector("#main-play-game");
+
+//Movimento e tiro da nave
+function flyShip(event) {
+  if (event.key === "ArrowUp") {
+    event.preventDefault();
+    moveUp();
+  } else if (event.key === "ArrowDown") {
+    event.preventDefault();
+    moveDown();
+  } else if (event.key === " ") {
+    event.preventDefault();
+    fireLaser();
+  }
+}
+
+//Subir
+function moveUp() {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue("top");
+  if (topPosition <= "0px") {
+    return;
+  } else {
+    let position = parseInt(topPosition);
+    position -= 50;
+    yourShip.style.top = `${position}px`;
+  }
+}
+
+//Descer
+function moveDown() {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue("top");
+  if (topPosition >= "510px") {
+    return;
+  } else {
+    let position = parseInt(topPosition);
+    position += 50;
+    yourShip.style.top = `${position}px`;
+  }
+}
+
+
+
+window.addEventListener("keydown", flyShip);
